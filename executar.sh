76ac2c9f1a340fd6cf637056537212ef61c6e46c
Breno Amorim Barbosa <130618894+tkLr7@users.noreply.github.com>
2025-06-14 00:17:26 +0000

    11

--- executar.sh
+++ executar.sh
@@ -10,10 +10,10 @@ wget https://repo1.maven.org/maven2/de/jflex/jflex/1.8.2/jflex-1.8.2.jar -O jfle
 wget https://repo1.maven.org/maven2/com/github/vbmacher/java-cup/11b-20160615/java-cup-11b-20160615.jar -O jcup.jar
 
 # Gerar o Analisador Léxico:
-java -cp jflex.jar:jcup.jar jflex.Main exemplo.flex
+java -cp jflex.jar:jcup.jar jflex.Main calculadora.flex
 
 # Gerar o Analisador Sintático:
-java -cp jcup.jar java_cup.Main -parser MeuParser exemplo.cup
+java -cp jcup.jar java_cup.Main -parser MeuParser calculadora.cup
 
 # Compilar as classes .java:
 javac -cp jcup.jar *.java
